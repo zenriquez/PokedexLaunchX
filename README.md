baf744bcd6f08808a14e73f317ababc5134ff5ce
zenriquez <zenriquez@uabc.edu.mx>
2022-03-27 20:54:12 -0600

    pokedex JS

---
+++ README.md
@@ -0,0 +1,1 @@
+# PokedexLaunchX
---
+++ js/main.js
@@ -0,0 +1,47 @@
+const fetchPokemon = () => {
+    const pokeNameInput = document.getElementById("pokeName");
+    let pokeName = pokeNameInput.value;
+    pokeName = pokeName.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+    fetch(url).then((res) => {
+        if (res.status != "200") {
+            console.log(res);
+            pokeImage("./img/pokemon-sad.gif")
+        }
+        else {
+            return res.json();
+        }
+    }).then((data) => {
+        if (data) {
+            console.log(data);
+            let pokeImg = data.sprites.front_default;
+            pokeImage(pokeImg);
+            console.log(pokeImg);
+        }
+    });
+}
+
+const pokeImage = (url) => {
+    const pokePhoto = document.getElementById("pokeImg");
+    pokePhoto.src = url;
+}
+
+const poke_container = document.getElementById('pokeImg');
+const pokemons_number = 150;
+const colors = {
+	fire: '#FDDFDF',
+	grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
+};
+
